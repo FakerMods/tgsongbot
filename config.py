576a3511058364fdf0514f26b967f2cd8a43cf14
FakerMods <fake1903jj@gmail.com>
2024-09-09 07:25:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Zaute Km <https://t.me/ZauteKm>
 
 import os
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("25033148"))
+API_HASH = os.getenv("9a6d023cc364b1077351a35b5b50b6e7")
+BOT_TOKEN = os.getenv("7339271220:AAHE27oEsM_I011kYEkBP8vxowUAuE_pGe4")
